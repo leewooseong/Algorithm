5abaf692d0bcd63fe764cf0616e40c3f42dac8c5
leewooseong <sitpo96@naver.com>
2021-12-07 13:34:31 +0900

    문제 3번 풀이 추가

---
+++ JS코딩100제/JS/1.배열의삭제.js
@@ -0,0 +1,10 @@
+// 다음 배열에서 400, 500를 삭제하는 code를 입력하세요.
+let nums = [100, 200, 300, 400, 500];
+
+nums.splice(3, 2);
+
+// 풀이2.
+// nums.pop();
+// nums.pop();
+
+console.log(nums);
---
+++ JS코딩100제/JS/3.변수의타입.js
@@ -0,0 +1,23 @@
+//다음 출력 값으로 올바른 것은?
+
+var arr = [100, 200, 300];
+console.log(typeof arr);
+
+// 1)  undefined
+//  - arr 변수가 선언 후 할당이 되었다. 따라서 답이 될 수 없다.
+//  - undefined는 선언은 했지만 할당해주지 않은 변수에 대해서 JS가 자동으로 할당해주는 값이다.
+
+// 2)  string
+//  - arr은 배열의 형태로 쓰였다.
+
+// 3)  number
+//  - arr은 배열의 형태로 쓰였다.
+
+// 4)  object
+//  - JS에선 number, string, boolean, null, undefined, symbol을 제외한 모든 타입은 object 타입을 가진다.
+
+// 정답: 4번
+
+// 추가로 공부할 것.
+// 1. 딥다이브 원시 값과 객체비교 내용 정리
+// 2. JS에서의 배열
