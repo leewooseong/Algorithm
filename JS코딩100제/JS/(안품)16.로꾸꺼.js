85ed5c6d8ef23d38fa2c7c28c977623fa2cdf4cc
leewooseong <sitpo96@naver.com>
2021-12-09 09:14:13 +0900

    15번까지 풀이

---
+++ JS코딩100제/JS/(안품)16.로꾸꺼.js
@@ -0,0 +1,25 @@
+// 문장이 입력되면 거꾸로 출력하는 프로그램을 만들어 봅시다.
+
+// 입출력
+
+// 입력 : 거꾸로
+// 출력 : 로꾸거
+
+let input = prompt("단어을 입력해주세요");
+
+function makeReverseString(word) {
+  // 따로 정의된 함수가 없는가?
+  // console.log(word.reverse());
+
+  let reverseWord = "";
+
+  // for spell of word {
+
+  // }
+}
+
+makeReverseString(input);
+
+// 공부할 것
+// reverse?
+// for in, for of
